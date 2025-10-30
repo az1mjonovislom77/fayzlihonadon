5be24f884cc22b2efd880084d66c5bef588689f3
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-10-30 11:29:13 +0500

    done

--- home/models.py
+++ home/models.py
@@ -25,7 +25,7 @@ class Home(models.Model):
     region = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return str(self.home_number)
+        return str(self.id)
 
 
 class HomeImage(models.Model):
