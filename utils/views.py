359d4b82626d9f22522b785836abb21e7a47e937
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-11-05 10:50:32 +0500

    done

--- utils/views.py
+++ utils/views.py
@@ -176,3 +176,10 @@ class LocationAPIView(APIView):
 
         serializer = LocationSerializer(location, many=True, context={'request': request})
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = LocationSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
